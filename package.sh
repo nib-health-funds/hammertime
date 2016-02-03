83de1ddb973bd99a209c1db3a79cf2eaf038854e
Ian Donaldson <idonaldson@dius.com.au>
2016-02-03 13:53:17 +1100

    Version zip files

--- package.sh
+++ package.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-zip -r start-hammertime start-hammertime.js node_modules
-zip -r stop-hammertime stop-hammertime.js node_modules
+zip -r start-hammertime-1.${BUILDKITE_BUILD_NUMBER} start-hammertime.js node_modules
+zip -r stop-hammertime-1.${BUILDKITE_BUILD_NUMBER} stop-hammertime.js node_modules
