47236108f1660de0eebcf33c9314455300d9e344
Ian Donaldson <idonaldson@dius.com.au>
2016-02-03 13:59:45 +1100

    Debug

--- package.sh
+++ package.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -xe
 
 zip -r start-hammertime-1.${BUILDKITE_BUILD_NUMBER} start-hammertime.js node_modules
 zip -r stop-hammertime-1.${BUILDKITE_BUILD_NUMBER} stop-hammertime.js node_modules
