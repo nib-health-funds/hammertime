0858e4c82b4509e30a8d2d1d71f8b4235d27a583
Ian Donaldson <idonaldson@dius.com.au>
2016-02-03 14:06:03 +1100

    Make sure there are no artefacts lying around

--- package.sh
+++ package.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -xe
 
+rm *.zip
 zip -r start-hammertime-1.${BUILDKITE_BUILD_NUMBER}.zip start-hammertime.js node_modules
 zip -r stop-hammertime-1.${BUILDKITE_BUILD_NUMBER}.zip stop-hammertime.js node_modules
