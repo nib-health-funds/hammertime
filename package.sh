6898aa59731ae4109b3ff3ce82eb009c38033919
Hailey Martin <h.martin@nib.com.au>
2016-03-22 09:41:56 +1100

    Force delete *.zip so we don't error on a zip not being present

--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
 
-rm *.zip
+rm -f *.zip
 zip -r start-hammertime-1.${BUILDKITE_BUILD_NUMBER}.zip start-hammertime.js node_modules
 zip -r stop-hammertime-1.${BUILDKITE_BUILD_NUMBER}.zip stop-hammertime.js node_modules
