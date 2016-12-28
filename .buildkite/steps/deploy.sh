7109fd106e1cf8b087044a26b617f57b6d12bc0a
Hailey Martin <h.martin@nib.com.au>
2016-12-28 02:40:55 +0000

    Fix name of npm script

--- .buildkite/steps/deploy.sh
+++ .buildkite/steps/deploy.sh
@@ -9,7 +9,7 @@ apt-get install -yqq jq awscli
 echo "--- :mocha: Running tests"
 npm test
 
-echo "--- :lambda: Deploying to Kaos"
+echo "--- :serverless: Deploying to Kaos"
 CREDENTIALS=$(aws sts assume-role --role-arn "arn:aws:iam::384553929753:role/deployer-artifacts-DeployerRole-1X76QQA70P2X2" --role-session-name="HammertimeDeploy")
 export AWS_ACCESS_KEY_ID=$(echo $CREDENTIALS | jq -r ".Credentials.AccessKeyId")
 export AWS_SECRET_ACCESS_KEY=$(echo $CREDENTIALS | jq -r ".Credentials.SecretAccessKey")
