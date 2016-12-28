8781b41128fa46150b15ffeeb5cf521a1467f8f0
Hailey Martin <h.martin@nib.com.au>
2016-12-28 04:22:21 +0000

    ????

--- handler.js
+++ handler.js
@@ -1,16 +1,8 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+const stopHammertime = require('./stop-hammertime');
+const startHammertime = require('./start-hammertime');
 
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+module.exports.stop = (event, context, callback) => {
+  console.log('Stop. Hammertime!');
 };
