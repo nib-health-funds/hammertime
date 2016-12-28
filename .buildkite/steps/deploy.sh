99978f429c1ba4bc2129148f5fe512708f8920f0
Hailey Martin <h.martin@nib.com.au>
2016-12-28 03:24:28 +0000

    Run destroy.sh in bash pls

--- .buildkite/steps/deploy.sh
+++ .buildkite/steps/deploy.sh
@@ -10,6 +10,8 @@ echo "--- :mocha: Running tests"
 npm test
 
 echo "--- :serverless: Deploying to Kaos"
+# This should really be moved outside the docker container and passed in.
+# Need to install jq on the buildkite agents for that.
 CREDENTIALS=$(aws sts assume-role --role-arn "arn:aws:iam::384553929753:role/deployer-artifacts-DeployerRole-1X76QQA70P2X2" --role-session-name="HammertimeDeploy")
 export AWS_ACCESS_KEY_ID=$(echo $CREDENTIALS | jq -r ".Credentials.AccessKeyId")
 export AWS_SECRET_ACCESS_KEY=$(echo $CREDENTIALS | jq -r ".Credentials.SecretAccessKey")
