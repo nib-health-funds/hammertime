3e7cc36d52409d225be3f061dde9bde3935b2f02
Hailey Martin <h.martin@nib.com.au>
2016-12-28 02:33:57 +0000

    Destroy step, deploy to lambdas bucket, slices,emojis

--- .buildkite/steps/deploy.sh
+++ .buildkite/steps/deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 set -eo pipefail
-echo "--- Installing dependencies"
-npm install --quiet --progress=false
+echo "--- :npm: Installing dependencies"
+npm install --quiet
 apt-get update -qq
 apt-get install -yqq jq awscli
 
-echo "--- Running tests"
+echo "--- :mocha: Running tests"
 npm test
 
-echo "--- Deploying to Kaos"
+echo "--- :lambda: Deploying to Kaos"
 CREDENTIALS=$(aws sts assume-role --role-arn "arn:aws:iam::384553929753:role/deployer-artifacts-DeployerRole-1X76QQA70P2X2" --role-session-name="HammertimeDeploy")
 export AWS_ACCESS_KEY_ID=$(echo $CREDENTIALS | jq -r ".Credentials.AccessKeyId")
 export AWS_SECRET_ACCESS_KEY=$(echo $CREDENTIALS | jq -r ".Credentials.SecretAccessKey")
