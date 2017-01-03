856e748ffb8d0c5cd24e37313e6ebc3f000d7ee9
Hailey Martin <h.martin@nib.com.au>
2017-01-03 23:31:57 +0000

    strict mode activate

--- test/start-hammertime.test.js
+++ test/start-hammertime.test.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const assert          = require('assert');
 const startHammertime = require('../start-hammertime');
 const AWS             = require('aws-sdk-mock');
--- test/stop-hammertime.test.js
+++ test/stop-hammertime.test.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const assert         = require('assert');
 const stopHammertime = require('../stop-hammertime');
 const AWS            = require('aws-sdk-mock');
