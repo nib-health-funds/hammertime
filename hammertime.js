f2b086b38ca0936fceb7b9542dbb10ec4872e14b
Hailey Martin <h.martin@nib.com.au>
2017-01-09 00:38:33 +0000

    Refactor

---
+++ hammertime.js
@@ -0,0 +1,104 @@
+'use strict';
+
+const instances = require('./src/instances')
+const asgs      = require('./src/asgs')
+
+module.exports.stop = (event, context, callback) => {
+  console.log('Stop. Hammertime!');
+  Promise.all([
+    stopInstances(),
+    stopASGs(),
+  ]).then(responses => {
+    console.log(responses);
+    callback(null, { message: 'Doneskies.' }, event);
+  }).catch(err => {
+    console.error(err);
+    callback(err);
+  });
+};
+
+module.exports.start = (event, context, callback) => {
+  console.log('Break it down!');
+  Promise.all([
+    startInstances(),
+    startASGs(),
+  ]).then(responses => {
+    console.log(responses);
+    callback(null, { message: 'Doneskies.' }, event);
+  }).catch(err => {
+    console.error(err);
+    callback(err);
+  });
+};
+
+function stopInstances() {
+  return new Promise((resolve, reject) => {
+    instances.listInstancesToStop()
+      .then(stoppableInstances => {
+        console.log("Found the following instances to shut down...");
+        console.log(stoppableInstances);
+        return instances.tagInstances;
+      })
+      .then(taggedInstances => {
+        console.log("Finished tagging instances. Moving on to stop them.");
+        console.log("Not stopping them for now!")
+        //return instances.stopInstances;
+        return taggedInstances;
+      })
+      .then(resolve)
+      .catch(reject);
+  });
+}
+
+function startInstances() {
+  return new Promise((resolve, reject) => {
+    instances.listInstancesToStart()
+      .then(startableInstances => {
+        console.log("Found the following instances to start up...");
+        console.log(startableInstances);
+        return instances.startInstances;
+      })
+      .then(startedInstances => {
+        console.log("Finished starting instances. Moving on to untag them.");
+        return instances.untagInstances;
+      })
+      .then(resolve)
+      .catch(reject);
+  });
+}
+
+function stopASGs() {
+  return new Promise((resolve, reject) => {
+    asgs.listASGsToStop()
+      .then(stoppableASGs => {
+        console.log("Found the following ASGs to spin down...");
+        console.log(stoppableASGs);
+        return asgs.tagASGs;
+      })
+      .then(taggedASGs => {
+        console.log("Finished tagging ASGs. Moving on to spin them down.");
+        console.log("Not spinning them down for now!")
+        //return asgs.stopASGs;
+        return taggedASGs;
+      })
+      .then(resolve)
+      .catch(reject);
+  });
+}
+
+function startASGs() {
+  return new Promise((resolve, reject) => {
+    asgs.listASGsToStart()
+      .then(startableASGs => {
+        console.log("Found the following ASGs to spin up...");
+        console.log(startableASGs);
+        return asgs.spinUpASGs;
+      })
+      .then(startedASGs => {
+        console.log("Finished spinning up ASGs. Moving on to untag them.");
+        return asgs.untagASGs;
+      })
+      .then(resolve)
+      .catch(reject);
+  });
+}
---
+++ test/asgs.test.js
@@ -0,0 +1,135 @@
+'use strict';
+
+const assert = require('assert');
+const asgs   = require('../src/asgs');
+const AWS    = require('aws-sdk-mock');
+
+describe('asgs', () => {
+
+  describe('listASGsToStop()', () => {
+
+    it('returns list of valid running asgs', () => {
+      const mockASGs = {
+        "AutoScalingGroups": [
+          {
+            "DesiredCapacity": 3,
+            "Tags": [
+              {
+                Key: "hammertime:canttouchthis",
+                Value: ""
+              }
+            ],
+            "AutoScalingGroupName": "cant-touch-this-asg",
+            "MinSize": 3,
+            "MaxSize": 3
+          },
+          {
+            "DesiredCapacity": 3,
+            "Tags": [],
+            "AutoScalingGroupName": "can-touch-this-asg",
+            "MinSize": 3,
+            "MaxSize": 3
+          }
+        ]
+      };
+      AWS.mock('AutoScaling', 'describeAutoScalingGroups', mockASGs);
+
+      return asgs.listASGsToStop()
+        .then(validAsgs => {
+          // assert.equal(validAsgs.length, 1);
+          assert.equal(validAsgs[0].AutoScalingGroupName, 'can-touch-this-asg');
+        });
+    });
+
+    it('returns an empty list if no asgs found', () => {
+      const mockASGs = { "AutoScalingGroups": [] };
+      AWS.mock('AutoScaling', 'describeAutoScalingGroups', mockASGs);
+
+      return asgs.listASGsToStop()
+        .then(validAsgs => {
+          assert.deepEqual(validAsgs, []);
+        });
+    });
+
+    afterEach(() => {
+      AWS.restore('AutoScaling', 'describeAutoScalingGroups');
+    });
+
+  });
+
+  describe('listASGsToStart()', () => {
+
+    it('returns list of asgs spun down by hammertime', () => {
+      const mockASGs = {
+        "AutoScalingGroups": [
+          {
+            "AutoScalingGroupName": "cant-touch-this-asg",
+            "MinSize": 0,
+            "MaxSize": 0,
+            "DesiredCapacity": 0,
+            "Tags": [
+              {
+                Key: "hammertime:canttouchthis",
+                Value: ""
+              },
+              {
+                Key: "stop:hammertime",
+                Value: ""
+              },
+              {
+                Key: "hammertime:originalASGSize",
+                Value: "1,1,1"
+              }
+            ],
+          },
+          {
+            "AutoScalingGroupName": "can-touch-this-asg",
+            "MinSize": 0,
+            "MaxSize": 0,
+            "DesiredCapacity": 0,
+            "Tags": [
+              {
+                Key: "stop:hammertime",
+                Value: ""
+              },
+              {
+                Key: "hammertime:originalASGSize",
+                Value: "1,1,1"
+              }
+            ]
+          },
+          {
+            "AutoScalingGroupName": "untouched-asg",
+            "MinSize": 1,
+            "MaxSize": 3,
+            "DesiredCapacity": 2,
+            "Tags": []
+          }
+        ]
+      };
+      AWS.mock('AutoScaling', 'describeAutoScalingGroups', mockASGs);
+
+      return asgs.listASGsToStart()
+        .then(validAsgs => {
+          assert.equal(validAsgs.length, 1);
+          assert.equal(validAsgs[0].AutoScalingGroupName, 'can-touch-this-asg');
+        });
+    });
+
+    it('returns an empty list if no asgs found', () => {
+      const mockASGs = { "AutoScalingGroups": [] };
+      AWS.mock('AutoScaling', 'describeAutoScalingGroups', mockASGs);
+
+      return asgs.listASGsToStart()
+        .then(validAsgs => {
+          assert.deepEqual(validAsgs, []);
+        });
+    });
+
+    afterEach(() => {
+      AWS.restore('AutoScaling', 'describeAutoScalingGroups');
+    });
+
+  });
+
+});
