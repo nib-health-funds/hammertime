f15fc0888da66a8e6a484f590ed707cf379bab2e
Matthew Turner <matthew.turner@nib.com.au>
2017-04-10 19:14:27 +1000

    Lint asg.js

--- src/asgs.js
+++ src/asgs.js
@@ -1,54 +1,23 @@
-'use strict';
-
-module.exports = {
-  listASGsToStop,
-  listASGsToStart,
-  tagASGs,
-  untagASGs,
-  stopASGs,
-  startASGs
-};
-
-const AWS          = require('aws-sdk');
+const AWS = require('aws-sdk');
 const promiseRetry = require('promise-retry');
 
-function listASGsToStop() {
-  return listTargetASGs(stoppableASG);
-}
+function followPages(resolve, reject, allAsgs, data) {
+  const autoscaling = new AWS.AutoScaling();
+  const params = {};
 
-function listASGsToStart() {
-  return listTargetASGs(startableASG);
-}
+  allAsgs.push(allAsgs, data.AutoScalingGroups);
 
-function tagASGs(asgs) {
-  const taggedASGs = asgs.map(asg => tagASG(asg));
-  return Promise.all(taggedASGs);
-}
-
-function untagASGs(asgs) {
-  const untaggedASGs = asgs.map(asg => untagASG(asg));
-  return Promise.all(untaggedASGs);
-}
-
-function stopASGs(asgs) {
-  const stoppedASGs = asgs.map(asg => spinDownASG(asg));
-  return Promise.all(stoppedASGs);
-}
-
-function startASGs(asgs) {
-  const startedASGs = asgs.map(asg => spinUpASG(asg));
-  return Promise.all(startedASGs);
-}
-
-function listTargetASGs(filter) {
-  return new Promise((resolve, reject) => {
-    getAllASGs()
-      .then(allASGs => {
-        const filteredASGs = allASGs.filter(filter);
-        resolve(filteredASGs);
+  if (data.NextToken) {
+    params.NextToken = data.NextToken;
+    autoscaling.describeAutoScalingGroups(params)
+      .promise()
+      .then((res) => {
+        followPages(resolve, reject, allAsgs, res);
       })
       .catch(reject);
-  });
+  } else {
+    resolve(allAsgs);
+  }
 }
 
 function getAllASGs() {
@@ -58,30 +27,24 @@ function getAllASGs() {
   return new Promise((resolve, reject) => {
     autoscaling.describeAutoScalingGroups(params)
       .promise()
-      .then(data => {
-        followPages(resolve, reject, [], data);
+      .then(data => followPages(resolve, reject, [], data))
+      .catch(reject);
+  });
+}
+
+function listTargetASGs(filter) {
+  return new Promise((resolve, reject) => {
+    getAllASGs()
+      .then((allASGs) => {
+        const filteredASGs = allASGs.filter(filter);
+        resolve(filteredASGs);
       })
       .catch(reject);
   });
 }
 
-function followPages(resolve, reject, allAsgs, data) {
-  const autoscaling = new AWS.AutoScaling();
-  const params = {};
-
-  allAsgs.push.apply(allAsgs, data.AutoScalingGroups);
-
-  if (data.NextToken) {
-    params.NextToken = data.NextToken;
-    autoscaling.describeAutoScalingGroups(params)
-      .promise()
-      .then(res => {
-        followPages(resolve, reject, allAsgs, res);
-      })
-      .catch(reject);
-  } else {
-    resolve(allAsgs)
-  }
+function hasTag(asg, target) {
+  return asg.Tags.some(tag => tag.Key === target);
 }
 
 function stoppableASG(asg) {
@@ -94,6 +57,14 @@ function startableASG(asg) {
   return hasTag(asg, 'stop:hammertime') && !hasTag(asg, 'hammertime:canttouchthis');
 }
 
+function listASGsToStop() {
+  return listTargetASGs(stoppableASG);
+}
+
+function listASGsToStart() {
+  return listTargetASGs(startableASG);
+}
+
 function tagASG(asg) {
   const autoscaling = new AWS.AutoScaling();
   const params = {
@@ -103,33 +74,36 @@ function tagASG(asg) {
         PropagateAtLaunch: false,
         ResourceId: asg.AutoScalingGroupName,
         ResourceType: 'auto-scaling-group',
-        Value: `${asg.MinSize},${asg.MaxSize},${asg.DesiredCapacity}`
+        Value: `${asg.MinSize},${asg.MaxSize},${asg.DesiredCapacity}`,
       },
       {
         Key: 'stop:hammertime',
         PropagateAtLaunch: false,
         ResourceId: asg.AutoScalingGroupName,
         ResourceType: 'auto-scaling-group',
-        Value: new Date().toISOString()
-      }
-    ]
+        Value: new Date().toISOString(),
+      },
+    ],
   };
 
   return new Promise((resolve, reject) => {
-    promiseRetry((retry, number) => {
-      return autoscaling.createOrUpdateTags(params)
+    promiseRetry((retry, number) => autoscaling.createOrUpdateTags(params)
         .promise()
-        .catch(err => {
+        .catch((err) => {
           if (err.code === 'Throttling') {
             console.warn(`Throttling the AWS API trying to tag ${asg.AutoScalingGroupName}. Backing off... (${number}/10)`);
             retry(err);
           }
-          throw(err);
-        });
-    })
-    .then(data => { resolve(asg) })
+          throw err;
+        }))
+    .then(() => resolve(asg))
     .catch(reject);
   });
+}
+
+function tagASGs(asgs) {
+  const taggedASGs = asgs.map(asg => tagASG(asg));
+  return Promise.all(taggedASGs);
 }
 
 function untagASG(asg) {
@@ -139,31 +113,34 @@ function untagASG(asg) {
       {
         Key: 'hammertime:originalASGSize',
         ResourceId: asg.AutoScalingGroupName,
-        ResourceType: 'auto-scaling-group'
+        ResourceType: 'auto-scaling-group',
       },
       {
         Key: 'stop:hammertime',
         ResourceId: asg.AutoScalingGroupName,
-        ResourceType: 'auto-scaling-group'
-      }
-    ]
+        ResourceType: 'auto-scaling-group',
+      },
+    ],
   };
 
   return new Promise((resolve, reject) => {
-    promiseRetry((retry, number) => {
-      return autoscaling.deleteTags(params)
+    promiseRetry((retry, number) => autoscaling.deleteTags(params)
         .promise()
-        .catch(err => {
+        .catch((err) => {
           if (err.code === 'Throttling') {
             console.warn(`Throttling the AWS API trying to untag ${asg.AutoScalingGroupName}. Backing off... (${number}/10)`);
             retry(err);
           }
-          throw(err);
-        });
-    })
-    .then(data => { resolve(asg) })
+          throw err;
+        }))
+    .then(() => resolve(asg))
     .catch(reject);
   });
+}
+
+function untagASGs(asgs) {
+  const untaggedASGs = asgs.map(asg => untagASG(asg));
+  return Promise.all(untaggedASGs);
 }
 
 function spinDownASG(asg) {
@@ -175,20 +152,27 @@ function spinDownASG(asg) {
   };
 
   return new Promise((resolve, reject) => {
-    promiseRetry((retry, number) => {
-      return autoscaling.updateAutoScalingGroup(params)
+    promiseRetry((retry, number) => autoscaling.updateAutoScalingGroup(params)
         .promise()
-        .catch(err => {
+        .catch((err) => {
           if (err.code === 'Throttling') {
             console.warn(`Throttling the AWS API trying to spin down ${asg.AutoScalingGroupName}. Backing off... (${number}/10)`);
             retry(err);
           }
-          throw(err);
-        });
-    })
-    .then(data => { resolve(asg) })
+          throw err;
+        }))
+    .then(() => resolve(asg))
     .catch(reject);
   });
+}
+
+function stopASGs(asgs) {
+  const stoppedASGs = asgs.map(asg => spinDownASG(asg));
+  return Promise.all(stoppedASGs);
+}
+
+function valueForKey(tags, key) {
+  return tags.find(tag => tag.Key === key).Value;
 }
 
 function spinUpASG(asg) {
@@ -202,30 +186,30 @@ function spinUpASG(asg) {
   };
 
   return new Promise((resolve, reject) => {
-    promiseRetry((retry, number) => {
-      return autoscaling.updateAutoScalingGroup(params)
+    promiseRetry((retry, number) => autoscaling.updateAutoScalingGroup(params)
         .promise()
-        .catch(err => {
+        .catch((err) => {
           if (err.code === 'Throttling') {
             console.warn(`Throttling the AWS API trying to spin up ${asg.AutoScalingGroupName}. Backing off... (${number}/10)`);
             retry(err);
           }
-          throw(err);
-        });
-    })
-    .then(data => { resolve(asg) })
+          throw err;
+        }))
+    .then(() => resolve(asg))
     .catch(reject);
   });
 }
 
-function hasTag(asg, target) {
-  return asg.Tags.some(tag => {
-    return (tag.Key === target);
-  });
+function startASGs(asgs) {
+  const startedASGs = asgs.map(asg => spinUpASG(asg));
+  return Promise.all(startedASGs);
 }
 
-function valueForKey(tags, key) {
-  return tags.find(tag => {
-    return tag.Key === key
-  }).Value;
-}
+module.exports = {
+  listASGsToStop,
+  listASGsToStart,
+  tagASGs,
+  untagASGs,
+  stopASGs,
+  startASGs,
+};
