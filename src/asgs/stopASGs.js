6804489f854498cae996c4972ee67515d60fc939
Kurt Gardiner <k.a.gardiner@gmail.com>
2017-05-03 21:57:29 +1000

    :table_tennis_paddle_and_ball: De:bug:

--- src/asgs/stopASGs.js
+++ src/asgs/stopASGs.js
@@ -9,6 +9,7 @@ function spinDownASG(asg) {
     MinSize: 0,
   };
 
+  console.log(`Spinning Down ASG ${asg.AutoScalingGroupName}.`);
   return retryWhenThrottled(() => autoscaling.updateAutoScalingGroup(params))
     .then(() => asg);
 }
