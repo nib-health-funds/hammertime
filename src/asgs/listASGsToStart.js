b141233fc926b829708e3e87ab8836fc8d5baef8
Matthew Turner <matthew.turner@nib.com.au>
2017-05-06 10:42:03 +1000

    WIP

--- src/asgs/listASGsToStart.js
+++ src/asgs/listASGsToStart.js
@@ -3,7 +3,13 @@ const listTargetASGs = require('./listTargetASGs');
 
 function startableASG(asg) {
   console.log(`Looking at ${asg.AutoScalingGroupName}, stop:hammertime tag is ${hasTag(asg, 'stop:hammertime')}, hammertime:canttouchthis is ${hasTag(asg, 'hammertime:canttouchthis')}`);
-  return hasTag(asg, 'stop:hammertime') && !hasTag(asg, 'hammertime:canttouchthis');
+  const hammerTimeTags = asg.Tags.filter(tag => /^hammertime:*/.test(tag.Key));
+
+  const canStart = hammerTimeTags.reduce((accum, currentTag) => {
+    return accum && analyseTag(currentTag.Tag);
+  });
+
+  return hasTag(asg, 'stop:hammertime') && !hasTag(asg, 'hammertime:canttouchthis') && canStart;
 }
 
 function listASGsToStart() {
