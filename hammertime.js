f08062118ea86c8c606cb9b50ca125af1dc07f9b
Matthew Turner <matthew.turner@nib.com.au>
2017-05-29 12:14:39 +1000

    Fix bug where dry run runtime env var was being checked for as truthy instead of string value

--- hammertime.js
+++ hammertime.js
@@ -2,7 +2,7 @@ const start = require('./src/start');
 const stop = require('./src/stop');
 
 // Try and use env var, otherwise default to false.
-const dryRun = process.env.hammerTimeDryRun || false;
+const dryRun = process.env.hammerTimeDryRun === 'true' || false;
 
 module.exports = {
   start: (event, context, callback) => start({ event, context, callback, dryRun }),
