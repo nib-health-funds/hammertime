aa29d9ce59e5f7cea010abe5c31fe383069e468a
Matthew Turner <matthew.turner@nib.com.au>
2017-12-11 11:48:29 +1100

    Offset cron hours by timezone

--- events.js
+++ events.js
@@ -1,29 +1,34 @@
 const operatingTimezones = require('./operatingTimezones');
 const isEnabled = require('./isEnabled').isEnabled;
 
+function offsetUTCHour(utcHour, utcTimezoneOffset) {
+  const convertedHour = utcHour + utcTimezoneOffset;
+
+  if (convertedHour > 23) {
+    return convertedHour - 23; // wrapped to next day
+  }
+
+  return convertedHour;
+}
+
 function stop() {
-    const stopCrons = operatingTimezones.map(timezone => {
-        return {
-            rate: 'cron(30 9 * * ? *)',
-            enabled: isEnabled()
-        }
-    });
-    
-    return {
-        schedule: stopCrons // CRONS for stopping
-    };
+  const stopCrons = operatingTimezones.map(timezone => ({
+    rate: `cron(30 ${offsetUTCHour(22, timezone)} * * ? *)`,
+    enabled: isEnabled(),
+  }));
+  return {
+    schedule: stopCrons, // CRONS for stopping
+  };
 }
 
 function start() {
-    const startCrons = operatingTimezones.map(timezone => {
-        return {
-            rate: 'cron(30 19 * * ? *)',
-            enabled: isEnabled()
-        }
-    });
-    return {
-        schedule: startCrons // CRONS for starting
-    };
+  const startCrons = operatingTimezones.map(timezone => ({
+    rate: `cron(30 ${offsetUTCHour(9, timezone)} * * ? *)`,
+    enabled: isEnabled(),
+  }));
+  return {
+    schedule: startCrons, // CRONS for starting
+  };
 }
 
 module.exports.stop = stop;
