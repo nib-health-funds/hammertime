c0db2bc2d323c73517a50f5d870acd167316ca06
Matthew Turner <matthew.turner@nib.com.au>
2018-01-31 17:59:41 +1100

    Debug env var

--- operatingTimezones.js
+++ operatingTimezones.js
@@ -1,5 +1,7 @@
 const timezones = process.env.HAMMERTIME_OPERATING_TIMEZONES || '[10]';
 
+console.log('Timezones to operate in: ', timezones);
+
 const parsed = JSON.parse(timezones);
 
 module.exports = parsed;
