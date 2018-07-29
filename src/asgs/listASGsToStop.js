30a097db7dd0775c07fb1c4c6d8ffba503bc0df2
Matthew Turner <matthew.turner@nib.com.au>
2018-07-29 11:18:46 +1000

    Add more info to log

--- src/asgs/listASGsToStop.js
+++ src/asgs/listASGsToStop.js
@@ -1,9 +1,10 @@
 const hasTag = require('../tags/hasTag');
 const listTargetASGs = require('./listTargetASGs');
 const canITouchThis = require('../tags/canITouchThis');
+const operatingTimezone = require('../tags/operatingTimezone');
 
 function stoppableASG(asg) {
-  console.log(`Looking at ${asg.AutoScalingGroupName}, stop:hammertime tag is ${hasTag(asg.Tags, 'stop:hammertime')}, hammertime:canttouchthis is ${hasTag(asg.Tags, 'hammertime:canttouchthis')}`);
+  console.log(`Looking at ${asg.AutoScalingGroupName}, stop:hammertime tag is ${hasTag(asg.Tags, 'stop:hammertime')}, hammertime:canttouchthis is ${hasTag(asg.Tags, 'hammertime:canttouchthis')}, operating timezone is ${operatingTimezone(asg.Tags)}`);
   return !hasTag(asg.Tags, 'stop:hammertime') && canITouchThis(asg.Tags);
 }
 
