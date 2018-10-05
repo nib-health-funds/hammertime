e0078739e78af5343dd3f3d177ab4d7453ddf0df
Matthew Turner <matthew.turner@nib.com.au>
2018-10-05 16:49:45 +1000

    Log comparison variables

--- src/operatingTimezone/isInOperatingTimezone.js
+++ src/operatingTimezone/isInOperatingTimezone.js
@@ -3,6 +3,8 @@ const operatingTimezone = require('../tags/operatingTimezone');
 function isInOperatingTimezone(currentOperatingTimezone) {
   return (tags) => {
     const resourceOperatingTimezone = operatingTimezone(tags);
+    console.log('Resource operating timezone', resourceOperatingTimezone);
+    console.log('Current operating timezone', currentOperatingTimezone);
     return resourceOperatingTimezone === currentOperatingTimezone;
   };
 }
