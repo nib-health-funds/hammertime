4f0e406db543ec2c9e7fd2bc76d965aa1d79da42
dyaxley <danielyaxley@gmail.com>
2019-10-17 17:52:30 +1100

    default to false

--- isWeekendsOff.js
+++ isWeekendsOff.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line arrow-body-style
 module.exports.isEnabled = () => {
-  return process.env.WEEKENDS_OFF
-    ? process.env.WEEKENDS_OFF === 'true'
+  return process.env.HAMMERTIME_WEEKENDS_OFF
+    ? process.env.HAMMERTIME_WEEKENDS_OFF === 'true'
     : 'false';
 };
