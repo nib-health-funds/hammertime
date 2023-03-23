cd6cc17d559786992043f5c31c2ff4ce9cb78132
Peter Crotty <peter.crotty@cmd.com.au>
2023-03-23 11:46:15 +1000

    Add more error handling

--- src/asgs/untagResumedASGs.js
+++ src/asgs/untagResumedASGs.js
@@ -10,21 +10,26 @@ function untagASG(asg) {
     ],
   };
 
-  return retryWhenThrottled(() => autoscaling.deleteTags(params))
-    .then(() => asg);
+  try {
+    return retryWhenThrottled(() => autoscaling.deleteTags(params))
+      .then(() => asg);
+  }
+  catch (err) {
+    console.log('untagASG - Broken Throttling here:' + err + ' ASG is:' + asg);
+    return err;
+  }
 }
 
 function untagResumedASGs(asgs) {
   const untaggedASGs = asgs.map(asg => untagASG(asg));
   try {
     data = Promise.all(untaggedASGs);
+    return data;
   }
     catch (err) {
-      console.log('untagResumedASGs - Broken Promise here:');
-      console.log(err);
+      console.log('untagResumedASGs - Broken Promise here:' + err);
       return err;
   }
-  return data
 }
 
 module.exports = untagResumedASGs;
