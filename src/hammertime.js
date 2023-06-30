9ccb3220c51b18cbf95805dcc8c7250edcfeebe8
katevucmd <kate.vu@cmd.com.au>
2023-06-30 14:43:30 +1000

    using env in lambda for dryrun

--- src/hammertime.js
+++ src/hammertime.js
@@ -1,14 +1,11 @@
 const start = require('./start');
 const stop = require('./stop');
 
-// Try and use env var, otherwise default to false.
-// TODO: set dryRun = true for testing
-// const dryRun = process.env.hammerTimeDryRun === 'true' || false;
-const dryRun = true;
+const dryRun = process.env.hammerTimeDryRun === 'true' || false;
 
 module.exports = {
   start: (event, context, callback) => start({ event, context, callback, dryRun }),
   // stop: (event, context, callback) => stop({ event, context, callback, dryRun }),
   //TODO: set dryrun = false for testing
-  stop: (event, context, callback) => stop({ event, context, callback, dryRun:false }),
+  stop: (event, context, callback) => stop({ event, context, callback, dryRun }),
 };
