d367000f2a3b46dbbe721579c6bbaceb3d479be5
katevucmd <kate.vu@cmd.com.au>
2023-07-10 18:12:30 +1000

    try catch

--- src/asgs/untagResumedASGs.js
+++ src/asgs/untagResumedASGs.js
@@ -1,28 +1,29 @@
-const AWS = require('aws-sdk');
-const retryWhenThrottled = require('../utils/retryWhenThrottled');
+const AWS = require("aws-sdk");
+const retryWhenThrottled = require("../utils/retryWhenThrottled");
 // const createTag = require('../utils/createTag');
-const deleteTagParams = require('../utils/deleteTagParams');
+const deleteTagParams = require("../utils/deleteTagParams");
 
 function untagASG(asg) {
   const autoscaling = new AWS.AutoScaling();
   const params = {
     Tags: [
-      deleteTagParams('stop:hammertime', asg.AutoScalingGroupName, 'auto-scaling-group'),
+      deleteTagParams(
+        "stop:hammertime",
+        asg.AutoScalingGroupName,
+        "auto-scaling-group"
+      ),
     ],
   };
-  return retryWhenThrottled(() => autoscaling.deleteTags(params))
-    .then(() => asg);
+  return retryWhenThrottled(() => autoscaling.deleteTags(params)).then(
+    () => asg
+  );
 }
 
 function untagResumedASGs(asgs) {
-  const untaggedASGs = asgs.map(asg => untagASG(asg));
-  try {
-    return Promise.all(untaggedASGs);
-  }
-    catch (err) {
-      console.log('untagResumedASGs - Broken Promise here:' + err);
-      return err;
-  }
+  const untaggedASGs = asgs.map((asg) => untagASG(asg));
+  return Promise.all(untaggedASGs).catch((error) => {
+    console.log("untagResumedASGs - Broken Promise here:", error);
+  });
 }
 
 module.exports = untagResumedASGs;
