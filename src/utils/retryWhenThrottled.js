726dc1e30ee54ca70c8b904d5878d340d4ecf642
Penny Zhang <penny.zhang@nib.com.au>
2023-09-07 10:44:29 +1000

    Update retryWhenThrottled.js

--- src/utils/retryWhenThrottled.js
+++ src/utils/retryWhenThrottled.js
@@ -3,7 +3,7 @@ const promiseRetry = require('promise-retry');
 module.exports = function retryWhenThrottled(func) {
   return promiseRetry((retry, number) => Promise.resolve(func())
     .catch((err) => {
-      console.log(err)
+      console.log('This is the output error', err)
       if (err.errorType === 'Throttling' || err.errorType === 'ThrottlingException') {
         console.warn(`Throttled by the AWS API. Backing off... (${number}/10)`);
         retry(err);
