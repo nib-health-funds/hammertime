4e87a1c881a4ae397cd0caa579335460c42e2c16
Penny Zhang <penny.zhang@nib.com.au>
2023-09-08 10:18:49 +1000

    debug error

--- src/utils/retryWhenThrottled.js
+++ src/utils/retryWhenThrottled.js
@@ -3,8 +3,8 @@ const promiseRetry = require('promise-retry');
 module.exports = function retryWhenThrottled(func) {
   return promiseRetry((retry, number) => Promise.resolve(func())
     .catch((err) => {
-      console.log('This is the output error', err.errorType)
-      if (err.errorType === 'Throttling' || err.errorType === 'ThrottlingException') {
+      console.log('This is the output error', err.ErrorType)
+      if (err.ErrorType === 'Throttling' || err.ErrorType === 'ThrottlingException') {
         console.warn(`Throttled by the AWS API. Backing off... (${number}/10)`);
         retry(err);
       }
